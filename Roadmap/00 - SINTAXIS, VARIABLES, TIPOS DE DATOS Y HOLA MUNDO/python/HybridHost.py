6cc9345b6707dfab2fcd6684f059ac96ef220283
Hybrid Host <145115480+DELACUESTA1@users.noreply.github.com>
2024-02-26 14:24:06 +0100

    HybridHost.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/HybridHost.py
@@ -0,0 +1,30 @@
+## SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO ##
+
+# 1 Comentario URL Oficial 
+# https://www.python.org/
+
+# 2 Tipos de comentarios
+"""
+Este es comentario "triple quoted" o multilinea.
+"""
+'''
+Este es un comentario "simple quoted"  o simple
+'''
+# 3 Creación de Variable y Constante
+# Creación
+my_variable = "Hybrid"
+my_constant = None
+# Comprobación de Cte. (opcional)
+try:
+    my_constant = "New Value"
+except:
+    print("Las constantes no son modificables")
+
+# 4 Creación de Variables Representando los Tipos de Datos
+my_string = "Hybrid_Host"
+my_int = 0
+my_float = 0.5
+my_bool = True
+
+# 5 Impresión de "Hola, [nombre de tu lenguaje]!"
+print("Hola, Python!")
